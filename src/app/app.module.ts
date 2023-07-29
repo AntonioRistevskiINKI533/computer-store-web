b32096fd6cafcbc638a8ab821403813eecee4f24
AntonioRistevskiINKI533 <antonio.ristevski@uklo.edu.mk>
2023-07-29 15:55:06 +0200

    mat-drawer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
